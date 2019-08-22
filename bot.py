15360ceb95742493ee2b44a6a5b43205af3882ce
Egor <egor.sierghieiev@mail.ru>
2019-08-23 00:23:57 +0300

    one more commit

--- bot.py
+++ bot.py
@@ -15,13 +15,11 @@ def show_jobs(message):
     with open('jobs.json') as file:
         response = ''
         jobs = json.load(file)
-        jobs = jobs[:3]
         for i, job in enumerate(jobs):
             job['description'] = job['description']
             # response += '%d. %s - %s\n\n%s\n\n\n' % (i + 1, job['title'], job['price'], job['description'])
-            response += f"{i+1}. {job['title']} - {job['price']}\n\n{job['description']}\n{job['link']}\n\n"
-            print(job['description'])
-    bot.send_message(message.chat.id, response)
+            response = f"{i+1}. {job['title']} - {job['price']}\n\n{job['description']}\n{job['link']}\n\n"
+            bot.send_message(message.chat.id, response)
 
 
 print('working')
